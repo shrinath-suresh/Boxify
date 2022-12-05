8ae9823857f7846f1010199f5d2f50d81a45d0a0
Shrinath Suresh <shrinath@ideas2it.com>
2022-12-05 23:20:55 +0530

    setup.py update
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- setup.py
+++ setup.py
@@ -7,9 +7,5 @@ setup(
     description="Package for format conversion and drawing bounding boxes",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    packages=find_packages(),
-    # Require MLflow as a dependency of the plugin, so that plugin users can simply install
-    # the plugin & then immediately use it with MLflow
-    # install_requires=["torchserve", "torch-model-archiver", "mlflow"],
-    # entry_points={"mlflow.deployments": "torchserve=mlflow_torchserve"},
+    packages=find_packages()
 )
